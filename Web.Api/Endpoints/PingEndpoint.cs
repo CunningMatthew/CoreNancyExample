605f94ced6be4c14e128cfe368f40287ef5af331
matthewc@iqmetrix.com <matthewc@iqmetrix.com>
2017-05-23 14:29:29 -0700

    Initial Commit

---
+++ Web.Api/Endpoints/PingEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace Web.Api.Endpoints
+{
+    public class PingEndpoint : NancyModule
+    {
+        public PingEndpoint()
+        {
+            Get("/ping", x => Ping());
+        }
+
+        private string Ping()
+        {
+            return "Hello World!";
+        }
+    }
+}
